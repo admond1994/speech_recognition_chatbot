5c2d533573fda5340b17d702ed827b84acba226c
Admond Lee <35163759+admond1994@users.noreply.github.com>
2019-01-19 22:37:45 +0800

    Add files via upload

---
+++ voice_recognition.py
@@ -0,0 +1,73 @@
+import speech_recognition as sr
+
+#%%
+'''
+   // Voice Recognition (Speech-to-Text) - Google Speech Recognition API
+   -> This API converts spoken text (microphone) into written text (Python strings)
+   -> Personal or testing purposes only
+   -> Generic key is given by default (it may be revoked by Google at any time)
+   -> If using API key, quota for your own key is 50 requests per day
+'''
+
+#%%
+
+def recognize_speech_from_mic(recognizer, microphone):
+    """Transcribe speech from recorded from `microphone`.
+
+    Returns a dictionary with three keys:
+    "success": a boolean indicating whether or not the API request was
+               successful
+    "error":   `None` if no error occured, otherwise a string containing
+               an error message if the API could not be reached or
+               speech was unrecognizable
+    "transcription": `None` if speech could not be transcribed,
+               otherwise a string containing the transcribed text
+    """
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source) # #  analyze the audio source for 1 second
+        audio = recognizer.listen(source)
+
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #   update the response object accordingly
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable/unresponsive"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
+
+#%%
+
+if __name__ == "__main__":
+    recognizer = sr.Recognizer()
+    mic = sr.Microphone(device_index=1)
+    response = recognize_speech_from_mic(recognizer, mic)
+    print('\nSuccess : {}\nError   : {}\n\nText from Speech\n{}\n\n{}' \
+          .format(response['success'],
+                  response['error'],
+                  '-'*17,
+                  response['transcription']))
+    
+    
